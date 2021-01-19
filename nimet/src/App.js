dbc6e63062135f9c44faf5c6c02363e5f647fb84
Kaarle Mattila <kaarlemattila@gmail.com>
2021-01-19 14:46:18 +0200

    added names.json file

--- nimet/src/App.js
+++ nimet/src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+const names = require('./names.json')
+const numbers = [1, 2, 3, 4, 5];
 
-function App() {
+
+const App = () => {
+  console.log('names: ', names)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tämä on nimilista...
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         <div>{names.names.map((name) =>
+          <li>{name.name}, {name.amount}</li>)}
+        </div> 
+
       </header>
     </div>
   );
